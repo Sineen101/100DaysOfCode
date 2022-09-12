b4b8210cb39ae86df76d0e627d9bdc5a30246b53
Sineen101 <sineensaleem111@gmail.com>
2022-09-13 00:03:14 +0500

    Day 26

--- US States Guess Game/main.py
+++ US States Guess Game/main.py
@@ -18,13 +18,8 @@ while keep_guessing:
     state = data[data["state"] == answer_state]
 
     if answer_state == "Exit":  # if the user types exit, stop the game
-        states_to_learn = []
-        # convert the state column to a list
-        state_list = data["state"].to_list()
-        for s in state_list:
-            if s not in guessed_states:
-                states_to_learn.append(s)
-
+        states_to_learn = [
+            state for state in data["state"].to_list() if state not in guessed_states]
         new_data = pandas.DataFrame(states_to_learn)
         new_data.to_csv("states_to_learn.csv")
         break
